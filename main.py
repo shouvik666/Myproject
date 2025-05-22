ae988d0bbaee0807e169d67848a935805c28e26c
shouvik666 <iamshouvik213@gmail.com>
2025-05-23 00:50:38 +0530

    new changes

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ b = int(b) #Expplicit TypeCasting
 print(a,b)
 a,b = b,a
 print(a,b)
-b -= 2
+b -= 3
 if (not(a<b) and not(a == b) and a is not b):
     for i in range(1,b):
         print(i,"True")
